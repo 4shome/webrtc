c10d3236b068080b78f2ac7be464dca0d62dd878
zefeng <zflin@camect.com>
2025-06-13 10:21:34 +0800

    Add the missing crc32c origin files.

---
+++ src/third_party/crc32c/src/include/crc32c/crc32c_config.h
@@ -0,0 +1,14 @@
+// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef CRC32C_CRC32C_CONFIG_H_
+#define CRC32C_CRC32C_CONFIG_H_
+
+// Define to 1 if building for a big-endian platform.
+#define BYTE_ORDER_BIG_ENDIAN 0
+
+// Define to 1 if CRC32C tests have been built with Google Logging.
+#define CRC32C_TESTS_BUILT_WITH_GLOG 1
+
+#endif  // CRC32C_CRC32C_CONFIG_H_
